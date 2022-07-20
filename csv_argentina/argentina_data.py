f9ed693cf34f8561dbb3767eefc9e4f2ce180ee4
Rosina Scampino <rosinascampino@gmail.com>
2022-07-20 17:16:43 -0400

    cleaned Argentina data

---
+++ csv_argentina/argentina_data.py
@@ -0,0 +1,26 @@
+import csv
+import os
+
+
+dir_argentina = '/Users/rosinascampino/Desktop/names_project/csv_argentina'
+arg_table = [['Name', 'Count', 'Year', 'Country']]
+
+for filename in os.listdir(dir_argentina):
+    if filename.endswith('.csv'):
+        fname = os.path.join(dir_argentina, filename)
+        with open(fname, 'r', encoding="utf-8") as file:
+            reader = csv.reader(file)
+            next(reader, None)
+            for row in reader:
+                name = row[0].strip().title()
+                count = int(row[1])
+                year = row[2]
+                country = 'Argentina'
+                new_line = [name, count, year, country]
+                arg_table.append(new_line)
+
+os.chdir('/Users/rosinascampino/Desktop/names_project/cleaned_data')
+
+with open('argentina_data.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerows(arg_table)
