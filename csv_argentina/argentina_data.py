f24f37a6450b7b8010a4582a71b6d4408e49fafc
Rosina Scampino <rosinascampino@gmail.com>
2022-07-20 17:40:27 -0400

    filtered values>= 50

--- csv_argentina/argentina_data.py
+++ csv_argentina/argentina_data.py
@@ -17,7 +17,7 @@ for filename in os.listdir(dir_argentina):
                 year = row[2]
                 country = 'Argentina'
                 new_line = [name, count, year, country]
-                # since dataset contains many names with a frequency value of one,
+                # since the dataset contains many names with a frequency value of one,
                 # we'll append those names which frequency value is equal or greater than 50
                 if count >= 50:
                     arg_table.append(new_line)
