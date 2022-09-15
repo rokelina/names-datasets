3626a90e3a68f3bc2b21a4b1b6d79111c17e7ea3
Rosina Scampino <rosinascampino@gmail.com>
2022-09-15 11:51:51 -0400

    wrote functions

--- spain/spain_data.py
+++ spain/spain_data.py
@@ -1,55 +1,40 @@
-from openpyxl import Workbook
 from openpyxl import load_workbook
 import csv
 import os
 from NameClasses import MaleNames, FemaleNames
 from typing import List
 
-"""write year in each file, load multiple workbooks, 
-open wb['TOTAL'], create custom objects, write to a csv"""
 
 os.chdir("/Users/rosinascampino/Desktop/names_project/spain")
 spain_dir = "/Users/rosinascampino/Desktop/names_project/spain"
 
 
-wbs = []
-
-for file in os.listdir(spain_dir):
-    if file.endswith('.xlsx'):
-        wb = load_workbook(os.path.join(spain_dir, file))
-        sheet = wb['TOTAL']
-        for row in sheet['C5:C105']:
-            file_year = (file.split('.')[0])
-            for cell in row:
-                cell.value = file_year
-
-        wbs.append(wb['TOTAL'])
-
-
-print(len(wbs))
-
-data = []
-
-for ws in wbs:
-    for row in ws.iter_rows(min_row=5, max_row=104, min_col=1, max_col=5, values_only=True):
-        data.append(list(row))
-
-print(len(data))
-
-# def extract_data(directory):
-#     for file in os.listdir(directory):
-#         if file.endswith('.xlsx'):
-#             file_year = (file.split('.')[0])
-#             data = []
-#             wb = load_workbook(filename=os.path.join(
-#                 directory, file), data_only=True)
-#             totals = wb['TOTAL']
-#             for row in totals.iter_rows(min_row=5, max_row=104, values_only=True):
-#                 data.append(list(row))
-
-#     return data
+def get_worksheets(directory_path):
+    worksheets = []
+    for file in os.listdir(directory_path):
+        if file.endswith('.xlsx'):
+            wb = load_workbook(os.path.join(directory_path, file))
+            sheet = wb['TOTAL']
+            for row in sheet['C5:C105']:
+                file_year = (file.split('.')[0])
+                for cell in row:
+                    cell.value = file_year
+            worksheets.append(wb['TOTAL'])
+    return worksheets
 
 
-# spain_names = extract_data(spain_dir)
+def get_names_data(worksheets):
+    data = []
+    for ws in worksheets:
+        for row in ws.iter_rows(min_row=5, max_row=104, min_col=1, max_col=5, values_only=True):
+            data.append(list(row))
+    return data
 
-# print(len(spain_names))
+
+def write_csv(list_of_data):
+    os.chdir('/Users/rosinascampino/Desktop/names_project/cleaned_data')
+    with open('spain_data.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['Male Name', 'Male Count', 'Year',
+                        'Female name', 'Female Count'])
+        writer.writerows(list_of_data)
